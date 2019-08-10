0f4a65b284b69d7bd9011cd5edfb8ebc554c84ad
Hendriksb1 <hendrik.schulze.boeing@gmail.com>
2019-08-10 16:42:57 +0200

    improved layout

--- src/assets/scripts/scrollto.js
+++ src/assets/scripts/scrollto.js
@@ -2,11 +2,11 @@
 
 $( document ).ready(function() {
 
-    $(".btn").click(function() {
+    $(".scrollto").click(function() {
         let a_href = $(this).attr('href');
         $([document.documentElement, document.body]).animate({
             scrollTop: $(a_href).offset().top
-        }, 2000);
+        }, 1000);
     });
 
 });
